4076ce362cfeae0c040946cacee08dbf2bab529f
andiaspin <andiaspin3@gmail.com>
2020-03-09 13:37:55 +0800

    Program TRIAL

---
+++ Trial/Form1.vb
@@ -0,0 +1,54 @@
+﻿Public Class Form1
+    Dim Win32MnClass As System.Management.ManagementClass
+    Dim processors As System.Management.ManagementObjectCollection
+    Dim Enkrip, Output, Inputan, hasilenkripsi, prosesor As String
+    Dim Panjang_Input As Integer
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Win32MnClass = New System.Management.ManagementClass("Win32_Processor")
+        processors = Win32MnClass.GetInstances()
+        For Each processor As System.Management.ManagementObject In processors
+            prosesor = (processor("ProcessorID").ToString())
+            TextBox1.Text = prosesor
+        Next
+        Inputan = prosesor
+        Panjang_Input = Len(Inputan)
+
+        For i = 1 To Panjang_Input
+            Enkrip = Mid(Inputan, i, 1)
+            Enkrip = Asc(Enkrip)
+            Enkrip = (Enkrip + 20) - 43
+            Enkrip = Chr(Enkrip)
+            Output = Output & Enkrip
+        Next i
+        hasilenkripsi = Output
+
+        If My.Computer.Registry.GetValue("HKEY_CURRENT_USER\" + hasilenkripsi, "Andi", "Aspin") Is Nothing Then
+
+            MessageBox.Show("Kode Aktivasi Tidak Terdeteksi", "Perhatian !!", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Else
+            Me.Hide()
+            Form2.Timer1.Enabled = False
+            Form2.ShowDialog()
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Me.Visible = False
+        Form2.ShowDialog()
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If TextBox2.Text = hasilenkripsi Then
+            My.Computer.Registry.SetValue("HKEY_CURRENT_USER\" + hasilenkripsi, "Andi", "Aspin")
+            MessageBox.Show("Kode Aktivasi Benar", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Me.Visible = False
+            Form2.Timer1.Enabled = False
+            Form2.ShowDialog()
+        Else
+            MsgBox("Kode Aktivasi Salah", MsgBoxStyle.Exclamation)
+        End If
+
+    End Sub
+End Class
---
+++ Trial/Form2.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form2
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Static iCount As Integer
+        If iCount = 50 Then
+            iCount = 0
+            Me.Dispose()
+            Form1.Dispose()
+        Else
+            iCount = iCount + 1
+        End If
+    End Sub
+
+    Private Sub Form2_FormClosing(ByVal sender As System.Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles MyBase.FormClosing
+        Me.Dispose()
+        Form1.Dispose()
+    End Sub
+End Class
